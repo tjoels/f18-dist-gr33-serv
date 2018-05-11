bdb45b97b56472bde41f8ce105274c53498586b2
JakobsenSWT <Jakobsen1608@hotmail.com>
2018-05-11 02:22:13 +0200

    Chat is done

--- src/galgeleg/logik/LobbyInterface.java
+++ src/galgeleg/logik/LobbyInterface.java
@@ -4,7 +4,7 @@ public interface LobbyInterface extends java.rmi.Remote {
 
     Boolean login(String username, String password)                 throws java.rmi.RemoteException;
 
-    void registrer(String username, ClientInterface client)  throws java.rmi.RemoteException;
+    void registrer(String username, ClientInterface client)         throws java.rmi.RemoteException;
 
     void sendToAll(String message, String from)                     throws java.rmi.RemoteException;
 
