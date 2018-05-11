fa3a507cef5732ce77b05d69159918dbbd0b53ef
Troels Just Christoffersen <troelsjc@gmail.com>
2018-05-11 17:00:19 +0200

    tried debugging hostname issue

--- src/galgeleg/GalgeServer.java
+++ src/galgeleg/GalgeServer.java
@@ -10,19 +10,20 @@ public class GalgeServer {
 
     public static void main(String args[]) throws Exception {
 
+        System.setProperty("java.rmi.server.hostname", "130.225.170.244");   // Tried with fix from
+        // https://stackoverflow.com/questions/15685686/java-rmi-connectexception-connection-refused-to-host-127-0-1-1
         LobbyInterface lobby = new Lobby();
         LocateRegistry.createRegistry(1076);
 
         try {
             //Naming.rebind("rmi://ubuntu4.saluton.dk:1076/galgeleg", lobby);
-            Naming.rebind("rmi://localhost:1076/galgeleg", lobby);
+            Naming.rebind("rmi://130.225.170.244:1076/galgeleg", lobby);
 
             System.err.println("Waiting for connection from clients.");
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Chat Server failed: " + e);
         }
 
     }
 
 }
-
